1b88a7f8d61eb9c20490ea0ac91174f604d327bd
gargchiraag <chiraag.garg@chordify.com>
2023-09-28 13:27:52 +0530

    Star Pattern printed

---
+++ index.js
@@ -0,0 +1,18 @@
+let name="chiraag"
+console.log(name);
+document.write("Hey this is javascript practise! How are you?");
+
+let num1=45;
+let num2=45;
+console.log(num1+num2);
+
+
+for(let i=0;i<10;i++)
+{
+    let a="";
+    for(let j=0;j<=i;j++)
+    {
+        a+=" * ";
+    }
+    console.log(a);
+}
